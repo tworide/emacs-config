abde2a27e5c17944331cfc8647c93a5674592578
Tor Ivar Johansen <toij@ifi.uio.no>
2012-02-11 21:44:41 +0100

    Added "Revert All Buffers Visiting a file" functions

--- toij/my-functions.el
+++ toij/my-functions.el
@@ -41,3 +41,13 @@ arg lines up."
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
+
+
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files.") )
